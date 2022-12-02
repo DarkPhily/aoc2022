66566820c8ec46624f798e73100b9efdcb0d869b
Philipp Bender <philipp.bender@hetzner.com>
2022-12-02 06:42:31 +0100

    optimized day1

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -1,9 +1,10 @@
-import numpy as np
+import timeit
 
+start_time = timeit.default_timer()
 all_elves_calories = []
 elves_calories = []
 
-with open("data.txt", 'r') as data:
+with open("./data.txt", 'r') as data:
     for line in data:
         if line == "\n":
             all_elves_calories.append(elves_calories)
@@ -12,9 +13,12 @@ with open("data.txt", 'r') as data:
             calories = int(line.strip("\n"))
             elves_calories.append(calories)
 
-total_calories_per_elve = [np.sum(elves_calories) for elves_calories in all_elves_calories]
+total_calories_per_elf = [sum(elves_calories) for elves_calories in all_elves_calories]
 
-print(f"Answer 1: {np.amax(total_calories_per_elve)}")
+print(f"Answer 1: {max(total_calories_per_elf)}")
 
-sorted_elves = np.sort(total_calories_per_elve)
-print(f"Answer 2: {np.sum(sorted_elves[-3:])}")
+total_calories_per_elf.sort()
+print(f"Answer 2: {sum(total_calories_per_elf[-3:])}")
+
+end_time = timeit.default_timer()
+print(end_time - start_time)
